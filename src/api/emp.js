0887946c24fd814c5f73c1054a708253cbaa377b
kunjinkaottt <1363265979@qq.com>
2026-03-05 17:09:18 +0800

    changes commit

--- src/api/emp.js
+++ src/api/emp.js
@@ -3,7 +3,7 @@ import request from "@/utils/request";
 // 条件查询员工
 export const getEmps = (name,gender,begin,end,page,pageSize) => request.get(`/emps?name=${name}&gender=${gender}&begin=${begin}&end=${end}&page=${page}&pageSize=${pageSize}`);
 
-// 新增员工 
+// 新增员工
 export const addEmp = (emp) => request.post("/emps", emp);
 
 // 根据id查员工
