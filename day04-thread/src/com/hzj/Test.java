f514941c75c02e1f703a1ff29f9db8862c2ae616
kunjinkaottt <1363265979@qq.com>
2026-03-26 11:19:59 +0800

    主线程 i->6

--- day04-thread/src/com/hzj/Test.java
+++ day04-thread/src/com/hzj/Test.java
@@ -4,7 +4,7 @@ public class Test {
     public static void main(String[] args) {
         Thread t1 = new MyThread();
         t1.start();//不能直接调用run ，调用run的话还是单线程，先执行run再执行主线程，只有调用start才是开辟一个新的线程去执行run
-        for (int i = 0; i < 5; i++){ // 如果这部分放在子线程start之前，则会先执行完这部分再去执行子线程start
+        for (int i = 0; i < 6; i++){ // 如果这部分放在子线程start之前，则会先执行完这部分再去执行子线程start
             System.out.println("主线程："+i);
         }
     }
